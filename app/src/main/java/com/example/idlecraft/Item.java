9d455adebcfbb6471bf8193355f09c409152d2a7
nlakritz <natejl123@gmail.com>
2019-10-17 16:10:50 -0700

    Changed variable name for clarity.

--- app/src/main/java/com/example/idlecraft/Item.java
+++ app/src/main/java/com/example/idlecraft/Item.java
@@ -2,22 +2,22 @@ package com.example.idlecraft;
 
 public class Item {
     private String name;
-    private int owned; // number of item owned
+    private int count; // number of item owned
     private int max;
     private int rate; // automation rate based on workers
     private boolean unlocked; // determines whether or not the item is available to the user
 
     public Item() {
         this.name = "Default";
-        this.owned = 0;
+        this.count = 0;
         this.max = 0;
         this.rate = 0;
         this.unlocked = true;
     }
 
-    public Item(String name, int owned, int max, int rate, boolean unlocked) {
+    public Item(String name, int count, int max, int rate, boolean unlocked) {
         this.name = name;
-        this.owned = owned;
+        this.count = count;
         this.max = max;
         this.rate = rate;
         this.unlocked = unlocked;
@@ -27,16 +27,16 @@ public class Item {
         return name;
     }
 
-    public int getOwned() {
-        return owned;
+    public int getCount() {
+        return count;
     }
 
     public int getMax() {
-        return owned;
+        return max;
     }
 
     public int getWorkers() {
-        return owned;
+        return rate;
     }
 
     public boolean getUnlocked() {
@@ -47,13 +47,11 @@ public class Item {
         this.name = name;
     }
 
-    public void setOwned(int owned) {
-        this.owned = owned;
+    public void setCount(int count) {
+        this.count = count;
     }
 
-    public void setMax(int max) {
-        this.max = max;
-    }
+    public void setMax(int max) { this.max = max; }
 
     public void setWorkers(int workers) {
         this.rate = workers;
