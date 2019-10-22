580fe9045887c5c11edafd451a890b9f6bf11d4d
Travis Kerns <50121655+tkerns522@users.noreply.github.com>
2019-10-22 16:36:20 -0700

    Fixed issue where crashes occurred whenever the edittext was updated inside the progress bar thread

--- app/src/main/java/com/example/idlecraft/ui/gather/GatherFragment.kt
+++ app/src/main/java/com/example/idlecraft/ui/gather/GatherFragment.kt
@@ -4,16 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.example.idlecraft.MainActivity
 import com.example.idlecraft.R
 import kotlinx.android.synthetic.main.fragment_gather.view.*
 import com.example.idlecraft.mechanics.*
+import kotlinx.android.synthetic.main.fragment_gather.*
 
 class GatherFragment : Fragment() {
 
     private lateinit var gatherViewModel: GatherViewModel
-    val Item = Item()
+    //val Item = Item()
+
+    private fun incrementEditText(edit : EditText) {
+        val str = edit.text.toString()
+        val quantity = str.substring(0, str.indexOf(" "))
+        val itemType = str.substring(str.indexOf(" "), str.length)
+        val incVal = 1 + (quantity.toInt())
+        edit.setText(incVal.toString() + itemType)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,15 +40,14 @@ class GatherFragment : Fragment() {
         view.button_g_sticks.setOnClickListener {
             // TODO: Delete this code and get the number of sticks from the inventory class instead
             // TODO: of the edittext text.
-            // TODO: Turn progress bar thread code into a function with the progress bar and time
-            // TODO: as parameters
+            // TODO: Turn progress bar thread code into a function with the progress bar as its
+            // TODO: parameter (so we can call this function on multiple progress bars at a time)
             // TODO: Increment the counter at the end of the progress bar instead of the beginning
             var progressBar = view.progress_g_sticks
+            var editSticks = view.edit_g_sticks
 
             // Create a thread that animates the progress bar and ensure that the gather button
             // cannot be clicked while currently gathering.
-
-
             if (progressBar.progress != 0) return@setOnClickListener
             Thread(Runnable {
                 var progress = 0
@@ -46,20 +57,17 @@ class GatherFragment : Fragment() {
                     try { Thread.sleep(16) } // 3 seconds
                     catch (e: InterruptedException) { e.printStackTrace() }
                 }
-                progressBar.progress = 0 // Reset progress bar
+                progressBar.progress = 0   // Reset progress bar
+
+                // runOnUiThread allows the thread to update UI objects
+                activity?.runOnUiThread {
+                    incrementEditText(editSticks)
+                }
             }).start()
 
-            // Update the data in the Item class
-            // TODO: Use these variables globally so that other files see the changes
-            Item.setCount(Item.getCount() + 1)
-            var sticks = Item.getCount()
-
-            // Increment the number of sticks on the GUI.
-            val str = view.edit_g_sticks.text.toString()
-            val substr = str.substring(0, str.indexOf(" "))
-            val incVal = 1 + (substr.toInt())
-            val newStr = sticks.toString() + " Sticks"
-            view.edit_g_sticks.setText(newStr)
+            // TODO: Update the data in the Item class
+            //Item.setCount(Item.getCount() + 1)
+            //var sticks = Item.getCount()
         }
         return view
     }
