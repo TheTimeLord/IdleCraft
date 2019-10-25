22878375b426da6fdf13e8071c3d76e594924aab
TheTimeLord <brianandrus2@gmail.com>
2019-10-24 18:29:51 -0700

    Added button to sell sticks in inv page

--- app/src/main/java/com/example/idlecraft/ui/inventory/InventoryFragment.kt
+++ app/src/main/java/com/example/idlecraft/ui/inventory/InventoryFragment.kt
@@ -9,10 +9,16 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.idlecraft.R
+import com.example.idlecraft.mechanics.*
+import com.example.idlecraft.MainActivity
+import kotlinx.android.synthetic.main.fragment_gather.view.*
+import kotlinx.android.synthetic.main.fragment_inventory.*
+import kotlinx.android.synthetic.main.fragment_inventory.view.*
 
 class InventoryFragment : Fragment() {
 
     private lateinit var inventoryViewModel: InventoryViewModel
+    var act: MainActivity? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,10 +26,31 @@ class InventoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         inventoryViewModel = ViewModelProviders.of(this).get(InventoryViewModel::class.java)
+
+        // Variables here
         val view = inflater.inflate(R.layout.fragment_inventory, container, false)
+        act = activity as MainActivity
+        val inv = act!!.inventory
+        var textMoney = view.text_money_count
+        val sticksItem = inv.getItemByName("sticks")
+        var money = inv.getMoney()
+
+        // Initializations
+        textMoney.setText("$" + money.toString())
 
         // Insert fragment code here
 
+        /** Sell Sticks Button **/
+        view.button_sell_sticks.setOnClickListener() {
+            if(sticksItem.getCount() > 0) {
+                sticksItem.decreaseCount(1)
+                inv.increaseMoney(3)
+                money = inv.getMoney()
+                text_money_count.setText("$" + money.toString())
+            }
+        }
+
         return view
     }
+
 }
