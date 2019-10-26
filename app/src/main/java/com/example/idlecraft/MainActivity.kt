7e84d5bc4f4abc742ee376501c3e5a993ecb4fae
Travis Kerns <50121655+tkerns522@users.noreply.github.com>
2019-10-25 17:47:48 -0700

    Cleaned up gathering code.

--- app/src/main/java/com/example/idlecraft/MainActivity.kt
+++ app/src/main/java/com/example/idlecraft/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.idlecraft
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.webkit.WebViewFragment
 import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -21,16 +24,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        // remove craft and shop tab initially
-        navView.getMenu().findItem(R.id.navigation_craft).isVisible = true
-        navView.getMenu().findItem(R.id.navigation_shop).isVisible =  true
-
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // Passing each menu ID as a set of Ids because each menu should be considered as
+        // top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_gather, R.id.navigation_craft, R.id.navigation_shop, R.id.navigation_inventory
+                R.id.navigation_gather,  R.id.navigation_craft,
+                R.id.navigation_shop,    R.id.navigation_inventory
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
@@ -45,17 +45,4 @@ class MainActivity : AppCompatActivity() {
         inventory.addItem(sticks)
     }
 
-    /*
-    fun showCraftBtn (v: View) {
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        navView.getMenu().findItem(R.id.navigation_craft).isVisible = true
-    }
-
-    fun showShopBtn (v: View) {
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        navView.getMenu().findItem(R.id.navigation_shop).isVisible = true
-        v.findViewById<Button>(R.id.show_shop_btn).isEnabled = false
-    }
-
-     */
 }
--- app/src/main/java/com/example/idlecraft/ui/gather/GatherFragment.kt
+++ app/src/main/java/com/example/idlecraft/ui/gather/GatherFragment.kt
@@ -1,10 +1,12 @@
 package com.example.idlecraft.ui.gather
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.idlecraft.MainActivity
@@ -18,21 +20,13 @@ class GatherFragment : Fragment() {
     private lateinit var gatherViewModel: GatherViewModel
     var act: MainActivity? = null
 
-    private fun incrementEditText(edit : EditText) {
-        val str = edit.text.toString()
-        val quantity = str.substring(0, str.indexOf(" "))
-        val itemType = str.substring(str.indexOf(" "), str.length)
-        val incVal = 1 + (quantity.toInt())
-        edit.setText(incVal.toString() + itemType)
-    }
-
+    // setEditText: Set the item quantity inside edit to newVal. The item name
+    // is appended to the quantity automatically.
     private fun setEditText(edit: EditText, newVal: Int) {
         val str = edit.text.toString()
-        val quantity = str.substring(0, str.indexOf(" "))
         val itemType = str.substring(str.indexOf(" "), str.length)
         edit.setText(newVal.toString() + itemType)
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,22 +35,18 @@ class GatherFragment : Fragment() {
     ): View? {
         gatherViewModel = ViewModelProviders.of(this).get(GatherViewModel::class.java)
         val view = inflater.inflate(R.layout.fragment_gather, container, false)
-        var editSticks = view.edit_g_sticks
         act = activity as MainActivity
-        var sticksItem = act!!.inventory.getItemByName("sticks");
-        setEditText(editSticks, sticksItem.count)
 
+        var editSticks = view.edit_g_sticks
+        var progressBar = view.progress_g_sticks
+        var sticksItem = act!!.inventory.getItemByName("sticks")
+        setEditText(editSticks, sticksItem.count)
 
         // Called when the gather sticks button is clicked. It causes the progress bar to
         // start, and once finished, increments the amount of sticks that the player has.
         view.button_g_sticks.setOnClickListener {
-            // TODO: Delete this code and get the number of sticks from the inventory class instead
-            // TODO: of the edittext text.
-            // TODO: Turn progress bar thread code into a function with the progress bar as its
-            // TODO: parameter (so we can call this function on multiple progress bars at a time)
-            // TODO: Increment the counter at the end of the progress bar instead of the beginning
-            var progressBar = view.progress_g_sticks
-
+            Log.v("clicked", "button has been clicked")
+            // TODO: Try to create the progress bar thread in the main activity so it persists
             // Create a thread that animates the progress bar and ensure that the gather button
             // cannot be clicked while currently gathering.
             if (progressBar.progress != 0) return@setOnClickListener
@@ -65,25 +55,18 @@ class GatherFragment : Fragment() {
                 while (progress < progressBar.max) {
                     progress += 1
                     progressBar.progress = progress
-                    try { Thread.sleep(16) } // 3 seconds
+                    try { Thread.sleep(32) } // 3 seconds
                     catch (e: InterruptedException) { e.printStackTrace() }
                 }
-                progressBar.progress = 0   // Reset progress bar
-
-                sticksItem.count += 1
+                progressBar.progress = 0  // Reset progress bar
+                sticksItem.count += 1     // Increment number of sticks
 
                 // runOnUiThread allows the thread to update UI objects
                 activity?.runOnUiThread {
-                    incrementEditText(editSticks)
+                    setEditText(editSticks, sticksItem.count);
                 }
             }).start()
-
-            // TODO: Update the data in the Item class
-            //Item.setCount(Item.getCount() + 1)
-            //var sticks = Item.getCount()
         }
         return view
     }
-
-
 }
