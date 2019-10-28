0a49664f995dc4f14f431de7c4c527aa001db4b9
Nathan Lakritz <njlakrit@ucsc.edu>
2019-10-28 10:07:29 -0700

    Initialize rocks in inventory

--- app/src/main/java/com/example/idlecraft/MainActivity.kt
+++ app/src/main/java/com/example/idlecraft/MainActivity.kt
@@ -44,6 +44,10 @@ class MainActivity : AppCompatActivity() {
         sticks.name = "sticks"
         sticks.max = 10
         inventory.addItem(sticks)
+        val rocks = Item()
+        sticks.name = "rocks"
+        sticks.max = 10
+        inventory.addItem(rocks)
     }
 
 }
