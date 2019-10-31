c300c4945a5bfd979582ecf93eae83da3e146c05
Saar Sayfan <saarsayfan@gmail.com>
2019-10-30 20:48:49 -0700

    add very ugly crafting UI for spear and test visibility with groups

--- app/src/main/java/com/example/idlecraft/ui/CraftFragment.kt
+++ app/src/main/java/com/example/idlecraft/ui/CraftFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.idlecraft.R
+import kotlinx.android.synthetic.main.fragment_craft.view.*
 
 class CraftFragment : Fragment() {
 
@@ -18,6 +19,10 @@ class CraftFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_craft, container, false)
+
+        view.button_hide_craft_spear.setOnClickListener {
+            view.group_craft_spear.visibility = View.GONE
+        }
 
         return view
     }
