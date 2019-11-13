e55cf5a2213b3f1d208e93cd5508016b7e4667fa
TheTimeLord <brianandrus2@gmail.com>
2019-11-13 10:05:51 -0800

    Merged Inventory Fragment thread code. Fixed positioning of gathering modules.

--- app/src/main/java/com/example/idlecraft/ui/InventoryFragment.kt
+++ app/src/main/java/com/example/idlecraft/ui/InventoryFragment.kt
@@ -1,7 +1,5 @@
 package com.example.idlecraft.ui
 
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -19,7 +17,7 @@ class InventoryFragment : Fragment() {
     // updateItemText: Update the TextView to display min/max quantity for an item and the
     // item's name.
     private fun updateItemText(text: TextView, item: Item) {
-        text.text = "  " + item.count.toString() + "/" + item.max.toString() + "\n  " + item.name
+        text.text = "  " + item.count.toString() + "/" + item.max.toString() + "\n " + item.name
     }
 
     // updateMoneyText: Update the TextView for money to newVal
@@ -32,74 +30,55 @@ class InventoryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        act = activity as MainActivity
-
-        // Setup preferences (for save/load state)
-        val prefs: SharedPreferences = act!!.getApplicationContext().getSharedPreferences("ICSave", 0)
-        val editor: SharedPreferences.Editor = prefs.edit()
-
         // General Inventory Setup
         val view = inflater.inflate(R.layout.fragment_inventory, container, false)
+        act = activity as MainActivity
+        act!!.saveInv()
         val inv = act!!.inventory
         val textMoney = view.text_inv_money
+        updateMoneyText(textMoney, inv.money)
 
         //==========================================================================================
         // Sticks Variables and Button Listeners
         //==========================================================================================
-
         val textSticks = view.text_inv_sticks_quan
         val sticksItem = inv.getItemByName("sticks")
+
         var sticksTradeQuan = 1
-        view.text_inv_sticks_trade_quan.text = sticksTradeQuan.toString()
+        val textTradeQuan = view.text_inv_sticks_trade_amount
+        textTradeQuan.text = sticksTradeQuan.toString()
 
         // Sell Sticks Button
         view.button_inv_sticks_sell.setOnClickListener() {
-            if(sticksItem.count >= sticksTradeQuan) {
-                inv.increaseMoney(sticksItem.sellValue * sticksTradeQuan)
-                sticksItem.decreaseCount(sticksTradeQuan)
-            }
-            else {
-                inv.increaseMoney(sticksItem.sellValue * sticksItem.count)
-                sticksItem.decreaseCount(sticksItem.count)
-            }
+            var actualTradeQuan = sticksTradeQuan
+            // If attempting to sell more than we have, sell all we have instead.
+            if (sticksItem.count <= actualTradeQuan)
+                actualTradeQuan = sticksItem.count
+            sticksItem.decreaseCount(actualTradeQuan)
+            inv.increaseMoney(sticksItem.sellValue * actualTradeQuan)
         }
         // Buy Sticks Button
         view.button_inv_sticks_buy.setOnClickListener() {
-            if(inv.money >= (sticksItem.buyValue * sticksTradeQuan)
-                         && (sticksItem.count + sticksTradeQuan) <= sticksItem.max) {
-                sticksItem.increaseCount(sticksTradeQuan)
-                inv.decreaseMoney(sticksItem.buyValue * sticksTradeQuan)
-            }
+            var actualTradeQuan = sticksTradeQuan
+            // If attempting to buy more than we can afford, buy all that we can afford instead.
+            if (inv.money <= sticksItem.buyValue * actualTradeQuan)
+                actualTradeQuan = inv.money / sticksItem.buyValue
+            // If attempting to buy more than we can hold, buy all that we can hold instead.
+            if (sticksItem.count + actualTradeQuan >= sticksItem.max)
+                actualTradeQuan = sticksItem.max - sticksItem.count
+            sticksItem.increaseCount(actualTradeQuan)
+            inv.decreaseMoney(sticksItem.buyValue * actualTradeQuan)
         }
 
         // Sticks Plus Button (increments the amount of sticks to buy/sell)
         view.button_inv_sticks_plus.setOnClickListener() {
             if (sticksTradeQuan < sticksItem.max) sticksTradeQuan += 1
-            view.text_inv_sticks_trade_quan.text = sticksTradeQuan.toString()
+            textTradeQuan.text = sticksTradeQuan.toString()
         }
         // Sticks Minus Button (decrements the amount of sticks to buy/sell)
         view.button_inv_sticks_minus.setOnClickListener() {
             if (sticksTradeQuan > 1) sticksTradeQuan -= 1
-            view.text_inv_sticks_trade_quan.text = sticksTradeQuan.toString()
-        }
-
-        view.button_save_state.setOnClickListener {
-            val iterator = inv.items.iterator()
-            iterator.forEach {
-                val keyName = "item_" + it.name
-                editor.putInt(keyName, it.count)
-            }
-            editor.putInt("money", inv.money)
-            editor.commit()
-        }
-
-        view.button_load_state.setOnClickListener {
-            val iterator = inv.items.iterator()
-            iterator.forEach {
-                val keyName = "item_" + it.name
-                it.count = prefs.getInt(keyName, -1)
-            }
-            inv.money = prefs.getInt("money", -1)
+            textTradeQuan.text = sticksTradeQuan.toString()
         }
 
         //==========================================================================================
