dff8d3b8bfa122d583f55c62a5722d068f5a65bf
Saar Sayfan <saarsayfan@gmail.com>
2019-11-12 23:35:53 -0800

    start implementing individual layouts for crafting ui modules and add sword

--- app/src/main/java/com/example/idlecraft/ui/CraftFragment.kt
+++ app/src/main/java/com/example/idlecraft/ui/CraftFragment.kt
@@ -1,16 +1,22 @@
 package com.example.idlecraft.ui
 
+import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.idlecraft.MainActivity
 import com.example.idlecraft.R
 import com.example.idlecraft.mechanics.Inventory
 import com.example.idlecraft.mechanics.Item
+import kotlinx.android.synthetic.main.craft_spear.view.*
 import kotlinx.android.synthetic.main.fragment_craft.view.*
+import org.w3c.dom.Text
 
 class CraftFragment : Fragment() {
     private var act: MainActivity? = null
@@ -39,6 +45,9 @@ class CraftFragment : Fragment() {
             req3.text = reqItem3.count.toString() + "/" + item.reqAmount3.toString()  + " " + item.req3
         else req3.text = ""
     }
+
+    // startProgressBar: Create the thread to handle the progress bar for a craftable item
+//    private fun startProgressBar() {}
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -100,6 +109,58 @@ class CraftFragment : Fragment() {
         view.button_craft_spear_minus.setOnClickListener {
             if (spearCraftCount > 1) spearCraftCount -= 1
             textSpearAmount.text = spearCraftCount.toString()
+        }
+
+        //==========================================================================================
+        // Sword Variables and Button Listeners
+        //==========================================================================================
+        val textSword = view.findViewById<TextView>(R.id.text_craft_sword_count)
+        val progressBarSword = view.findViewById<ProgressBar>(R.id.progress_craft_sword)
+        val swordItem = inv.getItemByName("sword")
+        updateItemText(textSword, swordItem)
+
+        var swordCraftCount = 1
+        val textSwordAmount = view.findViewById<TextView>(R.id.text_craft_sword_amount)
+        textSwordAmount.text = swordCraftCount.toString()
+
+        val textSwordReq1 = view.findViewById<TextView>(R.id.text_craft_sword_req1)
+        val textSwordReq2 = view.findViewById<TextView>(R.id.text_craft_sword_req2)
+        val textSwordReq3 = view.findViewById<TextView>(R.id.text_craft_sword_req3)
+        updateReqText(textSwordReq1, textSwordReq2, textSwordReq3, swordItem)
+
+
+        view.findViewById<ImageButton>(R.id.button_craft_sword).setOnClickListener {
+            var craftCount = inv.howManyCanCraft(swordItem)
+            if (swordCraftCount < craftCount) craftCount = swordCraftCount
+            if (progressBarSword.progress != 0 || swordItem.count >= swordItem.max || !inv.isCraftable(swordItem, craftCount))
+                return@setOnClickListener
+            Thread(Runnable {
+                var progress = 0
+                while (progress < progressBarSword.max) {
+                    progress += 1
+                    progressBarSword.progress = progress
+                    try { Thread.sleep(16) } // 3 seconds
+                    catch (e: InterruptedException) { e.printStackTrace() }
+                }
+                progressBarSword.progress = 0     // Reset progress bar
+                inv.craftItem(swordItem, craftCount)
+                // runOnUiThread allows the thread to update UI objects
+                activity?.runOnUiThread {
+                    updateItemText(textSword, swordItem)
+                    updateReqText(textSwordReq1, textSwordReq2, textSwordReq3, swordItem)
+                }
+            }).start()
+        }
+
+        // Plus Button
+        view.findViewById<ImageButton>(R.id.button_craft_sword_plus).setOnClickListener {
+            if (swordCraftCount < swordItem.max) swordCraftCount += 1
+            textSwordAmount.text = swordCraftCount.toString()
+        }
+        // Minus Button
+        view.findViewById<ImageButton>(R.id.button_craft_sword_minus).setOnClickListener {
+            if (swordCraftCount > 1) swordCraftCount -= 1
+            textSwordAmount.text = swordCraftCount.toString()
         }
 
         return view
