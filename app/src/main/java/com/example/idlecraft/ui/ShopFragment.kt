5a86688e80c7a4223a4d0379aad6e1828b72c915
Travis Kerns <50121655+tkerns522@users.noreply.github.com>
2019-11-24 21:20:00 -0800

    Added modular UI function for shop

--- app/src/main/java/com/example/idlecraft/ui/ShopFragment.kt
+++ app/src/main/java/com/example/idlecraft/ui/ShopFragment.kt
@@ -1,19 +1,38 @@
+//==================================================================================================
+// ShopFragment.kt
+//
+// Authors: Brian Andrus, Eduardo Zamora, Nathan Lakritz, Saar Sayfan, Travis Kerns
+//
+// Description: This file contains the implementation of a fragment that contains functionality
+// for in-game shop. The fragment is created upon opening and destroyed upon closing. A
+// number of button listeners are setup that interact with the underlying data structure that
+// contains all player data: Inventory.
+//==================================================================================================
 package com.example.idlecraft.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.idlecraft.MainActivity
 import com.example.idlecraft.R
+import com.example.idlecraft.mechanics.Inventory
 import com.example.idlecraft.mechanics.Item
+import kotlinx.android.synthetic.main.fragment_inventory.view.*
+import kotlinx.android.synthetic.main.fragment_shop.*
 import kotlinx.android.synthetic.main.fragment_shop.view.*
 
 class ShopFragment : Fragment() {
+    // Private Member Fields
     private var act: MainActivity? = null
+    private lateinit var inv: Inventory
 
+    //==============================================================================================
+    // TextView Updating Procedures
+    //==============================================================================================
     // updateInvText: Update the TextView for an item to display its max inventory count.
     private fun updateInvText(text: TextView, item : Item) {
         text.text = "         Max Inventory x" + item.max.toString()
@@ -39,7 +58,98 @@ class ShopFragment : Fragment() {
         text.text = "$" + newVal.toString()
     }
 
+    //==============================================================================================
+    // setupShopItemListeners: This procedure sets up and references all UI elements in the
+    // shop fragment for a particular item given its item name as a string. The UI elements are
+    // named in a uniform fashion so that they can be easily referenced in such fashion. This
+    // essentially eliminates redundancy in the code.
+    //==============================================================================================
+    private fun setupShopItemListeners(v: View, itemName: String) {
+        val item = inv.getItemByName(itemName)
+        val pkg = "com.example.idlecraft"
+        val money = v.text_shop_money
+        var tradeAmount = 1
 
+        // Declare strings to reference a set of UI elements for an item
+        val invAmountStr     = "text_shop_${itemName}_inv"
+        val rateAmountStr    = "text_shop_${itemName}_rate"
+        val invPriceStr      = "text_shop_${itemName}_inv_price"
+        val ratePriceStr     = "text_shop_${itemName}_rate_price"
+        val invIncStr        = "text_shop_${itemName}_inv_increase"
+        val rateIncStr       = "text_shop_${itemName}_rate_increase"
+        val tradeQuanStr     = "text_shop_${itemName}_trade_amount"
+        val buyInvButtonStr  = "button_shop_${itemName}_buy_inv"
+        val buyRateButtonStr = "button_shop_${itemName}_buy_rate"
+        val plusButtonStr    = "button_shop_${itemName}_plus"
+        val minusButtonStr   = "button_shop_${itemName}_minus"
+
+        // Use the UI strings to reference each UI element
+        val invAmount     = v.findViewById<TextView>(resources.getIdentifier(invAmountStr, "id", pkg))
+        val rateAmount    = v.findViewById<TextView>(resources.getIdentifier(rateAmountStr, "id", pkg))
+        val invPrice      = v.findViewById<TextView>(resources.getIdentifier(invPriceStr, "id", pkg))
+        val ratePrice     = v.findViewById<TextView>(resources.getIdentifier(ratePriceStr, "id", pkg))
+        val invInc        = v.findViewById<TextView>(resources.getIdentifier(invIncStr, "id", pkg))
+        val rateInc       = v.findViewById<TextView>(resources.getIdentifier(rateIncStr, "id", pkg))
+        val tradeQuan     = v.findViewById<TextView>(resources.getIdentifier(tradeQuanStr, "id", pkg))
+        val buyInvButton  = v.findViewById<ImageButton>(resources.getIdentifier(buyInvButtonStr, "id", pkg))
+        val buyRateButton = v.findViewById<ImageButton>(resources.getIdentifier(buyRateButtonStr, "id", pkg))
+        val plusButton    = v.findViewById<ImageButton>(resources.getIdentifier(plusButtonStr, "id", pkg))
+        val minusButton   = v.findViewById<ImageButton>(resources.getIdentifier(minusButtonStr, "id", pkg))
+
+        // Update the UI with current values upon entering the fragment
+        updateInvText(invAmount, item)
+        updateRateText(rateAmount, item)
+        updatePriceTexts(invPrice, ratePrice, item, tradeAmount)
+        updateIncreaseAmountTexts(invInc, rateInc, item, tradeAmount)
+        tradeQuan.text = tradeAmount.toString()
+        updateMoneyText(money, inv.money)
+
+        // Buy Inventory Button: Increases the player's max inventory space for an item.
+        buyInvButton.setOnClickListener {
+            // If attempting to buy more than we have money for, buy all we can instead.
+            var actualTradeAmount = tradeAmount
+            if (item.buyMax * tradeAmount >= inv.money)
+                actualTradeAmount = inv.money / item.buyMax
+
+            item.increaseMax(item.incInv * actualTradeAmount)
+            updateInvText(invAmount, item)
+            inv.money -= item.buyMax * actualTradeAmount
+            updateMoneyText(money, inv.money)
+        }
+
+        // Buy Rate Button: Increases the amount gathered of an item.
+        buyRateButton.setOnClickListener {
+            // If attempting to buy more than we have money for, buy all we can instead.
+            var actualTradeAmount = tradeAmount
+            if (item.buyRate * tradeAmount >= inv.money)
+                actualTradeAmount = inv.money / item.buyRate
+
+            item.rate += item.incRate * actualTradeAmount
+            updateRateText(rateAmount, item)
+            inv.money -= item.buyRate * actualTradeAmount
+            updateMoneyText(money, inv.money)
+        }
+
+        // Plus Button: increments the trade quantity.
+        plusButton.setOnClickListener {
+            tradeAmount += 1
+            tradeQuan.text = tradeAmount.toString()
+            updatePriceTexts(invPrice, ratePrice, item, tradeAmount)
+            updateIncreaseAmountTexts(invInc, rateInc, item, tradeAmount)
+        }
+        // Minus Button: decrements the trade quantity.
+        minusButton.setOnClickListener {
+            if (tradeAmount > 1) tradeAmount -= 1
+            tradeQuan.text = tradeAmount.toString()
+            updatePriceTexts(invPrice, ratePrice, item, tradeAmount)
+            updateIncreaseAmountTexts(invInc, rateInc, item, tradeAmount)
+        }
+    }
+
+    //==============================================================================================
+    // onCreateView: Called upon fragment creation. It sets up a reference to the global inventory
+    // object, then sets up button listeners by calling setupShopItemListeners for each shop item.
+    //==============================================================================================
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -48,74 +158,16 @@ class ShopFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_shop, container, false)
         act = activity as MainActivity
         act!!.saveInv()
-        act = activity as MainActivity
-        val inv = act!!.inventory
-        val textMoney = view.text_shop_money
-        updateMoneyText(textMoney, inv.money)
+        inv = act!!.inventory
 
-        // Update Thread: activate Shop
+        /*/ Update Thread: activate Shop
         act!!.updateThreadCrafting = false
-        act!!.updateThreadInventory = false
+        act!!.updateThreadInventory = false*/
 
-        //==========================================================================================
-        // Sticks Shop Variables and Button Listeners
-        //==========================================================================================
-        val textSticksMax = view.text_shop_sticks_inv
-        val textSticksRate = view.text_shop_sticks_rate
-        val textSticksInvPrice = view.text_shop_sticks_inv_price
-        val textSticksRatePrice = view.text_shop_sticks_rate_price
-        val sticksItem = inv.getItemByName("sticks")
-        val textSticksTradeAmount = view.text_shop_sticks_trade_amount
-        val textSticksInvInc = view.text_shop_sticks_inv_increase
-        val textSticksRateInc = view.text_shop_sticks_rate_increase
-
-        var sticksTradeAmount = 1
-        textSticksTradeAmount.text = sticksTradeAmount.toString()
-        updateInvText(textSticksMax, sticksItem)
-        updateRateText(textSticksRate, sticksItem)
-        updatePriceTexts(textSticksInvPrice, textSticksRatePrice, sticksItem, sticksTradeAmount)
-        updateIncreaseAmountTexts(textSticksInvInc, textSticksRateInc, sticksItem, sticksTradeAmount)
-
-        // Buy Max Inventory Button
-        view.button_shop_sticks_buy_inv.setOnClickListener {
-            // If attempting to buy more than we have money for, buy all we can instead.
-            var actualTradeAmount = sticksTradeAmount
-            if (sticksItem.buyMax * sticksTradeAmount >= inv.money)
-                actualTradeAmount = inv.money / sticksItem.buyMax
-
-            sticksItem.increaseMax(sticksItem.incInv * actualTradeAmount)
-            updateInvText(textSticksMax, sticksItem)
-            inv.money -= sticksItem.buyMax * actualTradeAmount
-            updateMoneyText(textMoney, inv.money)
+        val shopItems = arrayOf("sticks")
+        shopItems.forEach {
+            setupShopItemListeners(view, it)
         }
-        // Buy Rate Button
-        view.button_shop_sticks_buy_rate.setOnClickListener {
-            // If attempting to buy more than we have money for, buy all we can instead.
-            var actualTradeAmount = sticksTradeAmount
-            if (sticksItem.buyRate * sticksTradeAmount >= inv.money)
-                actualTradeAmount = inv.money / sticksItem.buyRate
-
-            sticksItem.rate += sticksItem.incRate * actualTradeAmount
-            updateRateText(textSticksRate, sticksItem)
-            inv.money -= sticksItem.buyRate * actualTradeAmount
-            updateMoneyText(textMoney, inv.money)
-        }
-
-        // Plus Button (increase trade amount)
-        view.button_shop_sticks_plus.setOnClickListener {
-            sticksTradeAmount += 1
-            textSticksTradeAmount.text = sticksTradeAmount.toString()
-            updatePriceTexts(textSticksInvPrice, textSticksRatePrice, sticksItem, sticksTradeAmount)
-            updateIncreaseAmountTexts(textSticksInvInc, textSticksRateInc, sticksItem, sticksTradeAmount)
-        }
-        // Minus Button (decrease trade amount)
-        view.button_shop_sticks_minus.setOnClickListener {
-            if (sticksTradeAmount > 1) sticksTradeAmount -= 1
-            textSticksTradeAmount.text = sticksTradeAmount.toString()
-            updatePriceTexts(textSticksInvPrice, textSticksRatePrice, sticksItem, sticksTradeAmount)
-            updateIncreaseAmountTexts(textSticksInvInc, textSticksRateInc, sticksItem, sticksTradeAmount)
-        }
-
         return view
     }
 }
